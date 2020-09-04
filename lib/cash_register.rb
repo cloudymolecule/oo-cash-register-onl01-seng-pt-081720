df7745cd199683f55d72513a51ff9d271339aaef
Dario Carlino <baires82@gmail.com>
2020-09-04 01:46:33 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,8 +12,8 @@ class CashRegister
   end
 
   def add_item(title, price, quantity = 1)
-    res = title
-    @its[res]
+    @its
+    binding.pry 
     @total = @total + (price * quantity)
 
   end
