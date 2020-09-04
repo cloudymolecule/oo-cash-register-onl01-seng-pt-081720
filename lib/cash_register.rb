80e1044c689f801438c41a918b5c4a62e97b491b
Dario Carlino <baires82@gmail.com>
2020-09-04 01:12:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,8 +16,8 @@ class CashRegister
   end
 
   def apply_discount
-    #binding.pry
-    if @discount > 0
+    @discount = 0
+    if @discount == 0
       #binding.pry
       @total = @total - ((@total / 100)* (@discount) )
       return "After the discount, the total comes to $#{@total}."
